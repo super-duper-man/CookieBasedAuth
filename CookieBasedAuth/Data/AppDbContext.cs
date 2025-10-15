93378a5adf2daa406d1861a39befac50217eb6d7
Danny <m.r.fouladi7@gmail.com>
2025-10-15 13:55:14 +0330

    base database configuration and Emplyees  entity created in database

---
+++ CookieBasedAuth/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CookieBasedAuth.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookieBasedAuth.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {}
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CookieBasedAuth/Models/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CookieBasedAuth.Models.Entities
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
