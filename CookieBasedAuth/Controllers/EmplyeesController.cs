da1a472fa95659df0d86977c5bb86ad93e66610b
Danny <m.r.fouladi7@gmail.com>
2025-10-15 14:29:20 +0330

    Employees Controller

---
+++ CookieBasedAuth/Controllers/EmplyeesController.cs
@@ -0,0 +1,26 @@
+﻿using CookieBasedAuth.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CookieBasedAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmplyeesController : ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+        public EmplyeesController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmplyees()
+        {
+            var employees = await this.dbContext.Employees.ToListAsync();
+
+            return Ok(employees);
+        }
+    }
+}
